6aefa8d020fb81e4893588991300f63186bf46fc
Beavya <AnnBezymya@gmail.com>
2025-12-03 13:13:53 +0700

    add comment

--- main/models.py
+++ main/models.py
@@ -48,3 +48,26 @@ class Post(models.Model):
 
     def __str__(self):
         return f'{self.title} — {self.author.username}'
+
+class Comment(models.Model):
+    post = models.ForeignKey(
+        Post,
+        on_delete=models.CASCADE,
+        related_name='comments',
+        verbose_name='Пост'
+    )
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        verbose_name='Автор'
+    )
+    content = models.TextField(verbose_name='Текст комментария')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата публикации')
+
+    class Meta:
+        verbose_name = 'Комментарий'
+        verbose_name_plural = 'Комментарии'
+        ordering = ['created_at']
+
+    def __str__(self):
+        return f'Комментарий от {self.author.username} к посту {self.post.id}'
