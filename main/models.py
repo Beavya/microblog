261bc754da5e4b46a92c238b44c81de4c78d7d9e
Beavya <AnnBezymya@gmail.com>
2025-12-03 10:03:03 +0700

    add model advuser

--- main/models.py
+++ main/models.py
@@ -1,3 +1,20 @@
+# main/models.py
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class AdvUser(AbstractUser):
+    avatar = models.ImageField(
+        upload_to='avatars/',
+        blank=True,
+        null=True,
+        verbose_name='Аватар'
+    )
+    bio = models.TextField(
+        max_length=500,
+        blank=True,
+        verbose_name='Информация о себе'
+    )
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
--- microblog/settings.py
+++ microblog/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -106,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'ru-ru'
 
-TIME_ZONE = 'TIME_ZONE = Asia/Tomsk'
+TIME_ZONE = 'Asia/Tomsk'
 
 USE_I18N = True
 
@@ -122,3 +122,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'main.AdvUser'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
